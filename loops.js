69602a44b7afdb38489230bf518e61632aebc1d3
David Craven <davidcraven@Davids-MacBook-Pro-2.local>
2016-05-10 21:46:01 -1000

    number2

--- loops.js
+++ loops.js
@@ -1,7 +1,23 @@
 // Do your work here
 var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
 
-for (i = 0; i < presidents.length; i++){
-	console.log('The value of i is ' + i);
-	console.log('The value at Index is ' + presidents[i]);
+// for (i = 0; i < presidents.length; i++){
+// 	console.log('The value of i is ' + i);
+// 	console.log('The value at Index is ' + presidents[i]);
+// }
+
+function printContent(array){
+	for (i = 0; i < array.length; i++){
+		console.log('The value of i is ' + i);
+		console.log('The value at Index is ' + presidents[i]);
+	}
 }
+printContent(presidents);
+
+
+var stringOfNumbers = '';
+
+for (i = 10; i <= 20; i++){
+	stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
